4b49b92e662187b4ae91c6f467b92e7fe5a0a488
PutrikaJuliyaS <putrikajuli@gmail.com>
2026-09-15 21:29:50 +0800

    Add files via upload

--- SK4.py
+++ SK4.py
@@ -14,36 +14,34 @@ buku = {
 
 #perulangan untuk menampilkan menu
 while True:
-    print("1. tampilkan data buku dan penerbit")
-    print("2. tampilkan data buku")
-    print("3. tampilkan data penerbit")
-    print("4. hapus data penerbit")
+    print("1. tampilkan data buku")
+    print("2. tampilkan data penerbit")
+    print("3. hapus data penerbit")
+    print("4. update data penulis")
     print("5. keluar")
     
     pilihan = input("pilih menu (1-5): ")
 
 #Tampilkan data buku ketika pengguna memilih menu tampilkan data
     if pilihan == "1":
-        print(buku)
-
-    elif pilihan == "2":
         print(buku["data_buku"])
 
-    elif pilihan == "3":
+    elif pilihan == "2":
         print(buku["data_penerbit"])
 
-    elif pilihan == "4":
+    elif pilihan == "3":
         del buku["data_penerbit"]
 
         print("Setelah Delete:")
         print(buku)
 
+    elif pilihan == "4":
+        update = input("ubah nama penulis:")
+        buku["data_buku"].update({"penulis" : {update}})
+
+        print("Setelah Update:")
+        print(buku)
+
     elif pilihan == "5":
         print("anda keluar")
         break
-
-#Ubah data penulis pada Dictionary
-buku["data_buku"].update({"penulis": "joanne rowling"})
-
-print("Setelah Update:")
-print(buku)
